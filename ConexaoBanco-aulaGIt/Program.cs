d5dfafeaa314e76345c46725bfae55b6b5e3cc26
Giovanna Vonrondov Bergamasco <vonrondovgiovanna820@gmail.com>
2023-05-30 15:29:47 -0400

    Tratamento de esceções

--- ConexaoBanco-aulaGIt/Program.cs
+++ ConexaoBanco-aulaGIt/Program.cs
@@ -1,6 +1,16 @@
-﻿Console.Write("Informe o primeiro valor: ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Informe o primeiro valor: ");
-int b = int.Parse(Console.ReadLine());
-int result = a / b;
-Console.WriteLine(result);
+﻿
+
+try
+{
+    Console.Write("Informe o primeiro valor: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Informe o primeiro valor: ");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine(result);
+
+}catch (Exception ex)
+{
+    Console.WriteLine("ALgo de errado não está certo!");
+}
+Console.ReadKey();
