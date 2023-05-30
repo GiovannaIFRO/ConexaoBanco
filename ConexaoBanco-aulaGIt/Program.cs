39b1b9a217c68b9bba224a4d45f83c3b2e70a874
Giovanna Vonrondov Bergamasco <vonrondovgiovanna820@gmail.com>
2023-05-30 15:03:42 -0400

    Inicio do projeto de divisão de dois números

---
+++ ConexaoBanco-aulaGIt/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Informe o primeiro valor: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Informe o primeiro valor: ");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+Console.WriteLine(result);
