d53b689c43ccc44f62757d5dfaecdc33ad32693b
Giovanna Vonrondov Bergamasco <vonrondovgiovanna820@gmail.com>
2023-06-05 17:08:31 -0400

    try e catch uso

--- ConexaoBanco-aulaGIt/Program.cs
+++ ConexaoBanco-aulaGIt/Program.cs
@@ -1,16 +1,25 @@
 ﻿
 
-try
+try //tratar as exeções evitando erros
 {
     Console.Write("Informe o primeiro valor: ");
     int a = int.Parse(Console.ReadLine());
     Console.Write("Informe o primeiro valor: ");
     int b = int.Parse(Console.ReadLine());
+
     int result = a / b;
     Console.WriteLine(result);
-
-}catch (Exception ex)
+}
+catch (DivideByZeroException ex)
 {
-    Console.WriteLine("ALgo de errado não está certo!");
+    Console.WriteLine("Não é permitido a divisão por zero " + ex.Message);
+}
+catch(InvalidCastException ex)
+{
+    Console.WriteLine($"conversão de string para a  letra não é permitida {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo! " + ex.Message);
 }
 Console.ReadKey();
